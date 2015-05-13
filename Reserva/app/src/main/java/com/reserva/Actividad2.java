15d2b6fc28ea6dddbea18a00f98297e2daa605cb
César Lara <laralacesar@gmail.com>
2015-05-13 09:46:21 -0500

    Seek bar state saved. Date status saved but its behaving weird

---
+++ Reserva/app/src/main/java/com/reserva/Actividad2.java
@@ -0,0 +1,55 @@
+package com.reserva;
+
+import android.app.Activity;
+import android.content.ContentValues;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Actividad2 extends Activity {
+
+	String nombre = "", fecha = "", hora = "";
+	int personas = 0, dolares = 0;
+	TextView muestraDatos;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.actividad2);
+
+		muestraDatos = (TextView) findViewById(R.id.muestraDatos);
+
+		Bundle recibe = new Bundle();
+		recibe = this.getIntent().getExtras();
+
+		nombre = recibe.getString("nombre");
+		personas = recibe.getInt("personas");
+		fecha = recibe.getString("fecha");
+		hora = recibe.getString("hora");
+        dolares = recibe.getInt("dolares");
+		muestraDatos.setText("Reservacion a nombre de:\n" + nombre + "\n" + personas
+				+ " personas\nFecha: " + fecha + "\nHora: " + hora + "\nDolares: " + dolares + "\n");
+
+	}
+
+    public void hacerOtraReserva(View v) {
+        Intent envia = new Intent(this, MainActivity.class);
+        finish();
+        startActivity(envia);
+    }
+
+    public void mandarCorreo(View correo)
+    {
+        Intent intent = new Intent(Intent.ACTION_SEND);
+        intent.setType("text/plain");
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Asunto: Prueba");
+        intent.putExtra(Intent.EXTRA_TEXT, String.format("Contenido del correo: Prueba %s", muestraDatos.getText().toString()));
+        intent.putExtra(Intent.EXTRA_EMAIL, new String[] { "eoropezag@ipn.mx"} );
+        startActivity(intent);
+    }
+
+}
